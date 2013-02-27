81a1ee9390356e4187856ee073bec3d113060da7
Philipp Klaus <philipp.klaus@gmail.com>
2013-02-27 02:47:27 +0100

    Create new connection if called in a different thread

--- filedict.py
+++ filedict.py
@@ -9,44 +9,51 @@ from collections import UserDict
 import pickle
 import sqlite3
 import hashlib
-
-class Solutions:
-    Sqlite3 = 0
+import threading
 
 class FileDict(UserDict):
     """A dictionary that stores its data persistantly in a file
 
     Options:
         filename - which file to use
-        connection - use an existing connection instead of a filename (overrides filename)
         table - which table name to use for storing data (default: 'dict')
-    
+
     """
 
-    def __init__(self, filename=None, solution=Solutions.Sqlite3, **options):
-        assert solution == Solutions.Sqlite3, "Only sqlite3 is supported right now"
-        try:
-            self.__conn = options.pop('connection')
-        except KeyError:
-            if not filename:
-                raise ValueError("Must provide 'connection' or 'filename'")
-            self.__conn = sqlite3.connect(filename)
+    def __init__(self, filename=None, **options):
+        if not filename:
+            raise ValueError("Must provide 'connection' or 'filename'")
+        self._filename = filename
 
-        self.__tablename = options.pop('table', 'dict')
-        
+        self._connections = dict()
+        conn = self._conn
+
+        self._tablename = options.pop('table', 'dict')
+
         self._nocommit = False
 
         assert not options, "Unrecognized options: %s" % options
 
-        self.__conn.execute('CREATE TABLE IF NOT EXISTS %s (id integer primary key, hash integer, key blob, value blob);'%self.__tablename)
-        self.__conn.execute('CREATE INDEX IF NOT EXISTS %s_index ON %s(hash);' % (self.__tablename, self.__tablename))
-        self.__conn.commit()
+        self._execute('CREATE TABLE IF NOT EXISTS %s (id integer primary key, hash integer, key blob, value blob);'%self._tablename)
+        self._execute('CREATE INDEX IF NOT EXISTS %s_index ON %s(hash);' % (self._tablename, self._tablename))
+        self._commit()
+
+    @property
+    def _conn(self):
+        try:
+            return self._connections[threading.current_thread().ident]
+        except KeyError:
+            self._connections[threading.current_thread().ident] = sqlite3.connect(self._filename)
+            return self._connections[threading.current_thread().ident]
+
+    def _execute(self, *args, **kwargs):
+        return self._conn.execute(*args, **kwargs)
 
     def _commit(self):
         if self._nocommit:
             return
 
-        self.__conn.commit()
+        self._conn.commit()
 
     def __pack(self, value):
         return pickle.dumps(value, 3)
@@ -60,7 +67,7 @@ class FileDict(UserDict):
         return hash % 0x7FFFFFFF
 
     def __get_id(self, key):
-        cursor = self.__conn.execute('SELECT key,id FROM %s WHERE hash=?;'%self.__tablename, (self.__hash(key),))
+        cursor = self._execute('SELECT key,id FROM %s WHERE hash=?;'%self._tablename, (self.__hash(key),))
         for k,id in cursor:
             if self.__unpack(k) == key:
                 return id
@@ -68,7 +75,7 @@ class FileDict(UserDict):
         raise KeyError(key)
 
     def __getitem__(self, key):
-        cursor = self.__conn.execute('SELECT key,value FROM %s WHERE hash=?;'%self.__tablename, (self.__hash(key),))
+        cursor = self._execute('SELECT key,value FROM %s WHERE hash=?;'%self._tablename, (self.__hash(key),))
         for k,v in cursor:
             if self.__unpack(k) == key:
                 return self.__unpack(v)
@@ -77,14 +84,14 @@ class FileDict(UserDict):
 
     def __setitem(self, key, value):
         value_pickle = self.__pack(value)
-        
+
         try:
             id = self.__get_id(key)
-            cursor = self.__conn.execute('UPDATE %s SET value=? WHERE id=?;'%self.__tablename, (value_pickle, id) )
+            cursor = self._execute('UPDATE %s SET value=? WHERE id=?;'%self._tablename, (value_pickle, id) )
         except KeyError:
             key_pickle = self.__pack(key)
-            cursor = self.__conn.execute('INSERT INTO %s (hash, key, value) values (?, ?, ?);'
-                    %self.__tablename, (self.__hash(key), key_pickle, value_pickle) )
+            cursor = self._execute('INSERT INTO %s (hash, key, value) values (?, ?, ?);'
+                    %self._tablename, (self.__hash(key), key_pickle, value_pickle) )
 
         assert cursor.rowcount == 1
 
@@ -94,7 +101,7 @@ class FileDict(UserDict):
 
     def __delitem__(self, key):
         id = self.__get_id(key)
-        cursor = self.__conn.execute('DELETE FROM %s WHERE id=?;'%self.__tablename, (id,))
+        cursor = self._execute('DELETE FROM %s WHERE id=?;'%self._tablename, (id,))
         if cursor.rowcount <= 0:
             raise KeyError(key)
 
@@ -107,13 +114,13 @@ class FileDict(UserDict):
         self._commit()
 
     def __iter__(self):
-        return (self.__unpack(x[0]) for x in self.__conn.execute('SELECT key FROM %s;'%self.__tablename) )
+        return (self.__unpack(x[0]) for x in self._execute('SELECT key FROM %s;'%self._tablename) )
     def keys(self):
         return iter(self)
     def values(self):
-        return (self.__unpack(x[0]) for x in self.__conn.execute('SELECT value FROM %s;'%self.__tablename) )
+        return (self.__unpack(x[0]) for x in self._execute('SELECT value FROM %s;'%self._tablename) )
     def items(self):
-        return (list(map(self.__unpack, x)) for x in self.__conn.execute('SELECT key,value FROM %s;'%self.__tablename) )
+        return (list(map(self.__unpack, x)) for x in self._execute('SELECT key,value FROM %s;'%self._tablename) )
 
     def __contains__(self, key):
         try:
@@ -123,15 +130,15 @@ class FileDict(UserDict):
             return False
 
     def __len__(self):
-        return self.__conn.execute('SELECT COUNT(*) FROM %s;' % self.__tablename).fetchone()[0]
+        return self._execute('SELECT COUNT(*) FROM %s;' % self._tablename).fetchone()[0]
 
     def __del__(self):
         try:
-            self.__conn
+            self._conn
         except AttributeError:
             pass
         else:
-            self.__conn.commit()
+            self._commit()
 
     @property
     def batch(self):
